254eb7d2b806178ab26225e46435a010f3428b97
Tracys <jimi1966@msn.com>
2024-02-10 22:12:48 -0700

    Update make-change-function-in.js

--- make-change-function-in.js
+++ make-change-function-in.js
@@ -1,1 +1,20 @@
+function makeChange(amount, coins) {
+    // Sort coins in descending order
+    coins.sort((a, b) => b - a);
+    
+    let change = [];
+    
+    for (let i = 0; i < coins.length; i++) {
+        while (amount >= coins[i]) {
+            change.push(coins[i]);
+            amount -= coins[i];
+        }
+    }
+    
+    return change;
+}
 
+// Example usage:
+const amount = 47;
+const coins = [25, 10, 5, 1];
+console.log(makeChange(amount, coins)); // Output: [25, 10, 10, 1, 1]
